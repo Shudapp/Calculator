3802401d51ae07c680006688b62307c886910caf
Shudapp <dragosandrei9321@gmail.com>
2025-04-18 18:07:03 +0300

    Added calculator functionality for integers

--- src/Interfata.java
+++ src/Interfata.java
@@ -1,18 +1,27 @@
 //String car = "$$$+789-456*123/$0.=";
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Interfata {
+public class Interfata extends JFrame implements ActionListener {
     //JFrame frame = new JFrame();
     //JPanel panel = new JPanel();
+    String currentop;
+    String num1, num2;
+    int actualnum1, actualnum2;
+    int resultnum;
+    String result;
+    JTextField display = new JTextField(16);
+
     public Interfata() {
          //frame
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Calculator");
+        //frame.setLayout(new FlowLayout());
 
         //display field
-        JTextField display = new JTextField();
         display.setEditable(false);
         display.setHorizontalAlignment(JTextField.RIGHT);
         display.setFont(display.getFont().deriveFont(20f));
@@ -23,7 +32,9 @@ public class Interfata {
         JPanel buttons = new JPanel(new GridLayout(4,4,5,5));
         String[] labels = {"7", "8", "9", "+", "4", "5", "6", "-", "1", "2", "3", "*", "/" , "0", ".", "="};
         for(String text : labels){
-            buttons.add(new JButton(text));
+            JButton b = new JButton(text);
+            b.addActionListener(this);
+            buttons.add(b);
         }
         buttons.setBorder(BorderFactory.createEmptyBorder(50, 50, 50, 50));
 
@@ -32,5 +43,52 @@ public class Interfata {
 
         frame.pack();
         frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String word = e.getActionCommand();
+        if(word.equals("+") || word.equals("-") || word.equals("*") || word.equals("/")){
+            currentop = word;
+            if(num2 == null){
+                num2 = "0";
+            }
+        } else if(word.equals("=")){
+            //currentop = "=";
+            if(num2 == null) {
+                num2 = "0";
+            }
+            actualnum2 = Integer.parseInt(num2);
+            actualnum1 = Integer.parseInt(num1);
+            if(currentop.equals("+")) {
+                resultnum = actualnum1 + actualnum2;
+            } else if(currentop.equals("-")) {
+                resultnum = actualnum1 - actualnum2;
+            } else if(currentop.equals("*")) {
+                resultnum = actualnum1 * actualnum2;
+            } else if(currentop.equals("/")) {
+                resultnum = actualnum1 / actualnum2;
+            }
+            result = Integer.toString(resultnum);
+            num1 = result;
+            num2 = null;
+            display.setText(num1);
+        } else {
+            if(num2 == null){
+                if(num1 == null){
+                    num1 = word;
+                } else {
+                    num1 = num1 + word;
+                }
+                display.setText(num1);
+            } else {
+                if(num2 =="0"){
+                    num2="";
+                }
+                num2 = num2 + word;
+                display.setText(num2);
+            }
+        }
+        //System.out.println(num1 + " " + num2 + '\n');
     }
 }
